88ffe1e524a786ddcda8e29f2e4f2748d7b3d893
vfelipat <vfelipat@inmobitec.pe>
2025-02-28 19:00:56 -0500

    .

--- WebFPRTest/obj/Release/net6.0/WebFPRTest.AssemblyInfo.cs
+++ WebFPRTest/obj/Release/net6.0/WebFPRTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFPRTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aac1f6a78e02b1ae9bacc720e35af5346991a8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFPRTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFPRTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
